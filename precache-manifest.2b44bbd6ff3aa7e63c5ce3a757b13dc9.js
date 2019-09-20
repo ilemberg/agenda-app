0e94ce052defd637dde519a02d486cf22f1933b7
Ilya Lemberg <ilyalemberg@cvpcorp.com>
2019-09-20 13:22:14 -0400

    Updates

---
+++ precache-manifest.2b44bbd6ff3aa7e63c5ce3a757b13dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ada1c86d47990baa25dd6354f904010",
+    "url": "/agenda-app/index.html"
+  },
+  {
+    "revision": "eda8f0301d4c8aab2ba5",
+    "url": "/agenda-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd4614d35d44b102e498",
+    "url": "/agenda-app/static/js/2.9405f3f8.chunk.js"
+  },
+  {
+    "revision": "eda8f0301d4c8aab2ba5",
+    "url": "/agenda-app/static/js/main.d2d0a60e.chunk.js"
+  },
+  {
+    "revision": "def6acb561564d7701d5",
+    "url": "/agenda-app/static/js/runtime-main.8463842f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/agenda-app/static/media/logo.25bf045c.svg"
+  }
+]);
